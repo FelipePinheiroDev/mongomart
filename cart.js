406a85a7a053810dcc36b2aee7ef675348259bf4
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2017-07-18 13:03:32 -0300

    Lab 5

--- cart.js
+++ cart.js
@@ -38,19 +38,11 @@ function CartDAO(database) {
         *
         */
 
-        var userCart = {
-            userId: userId,
-            items: []
-        }
-        var dummyItem = this.createDummyItem();
-        userCart.items.push(dummyItem);
-
-        // TODO-lab5 Replace all code above (in this method).
-
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the userCart to the
-        // callback.
-        callback(userCart);
+        this.db
+            .collection("cart")
+            .findOne({ userId }, function (err, userCart) {
+                callback(userCart);
+            });
     }
 
 
